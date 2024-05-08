e6bcab12c9b78c5fba0ba3790ebf91c5f855be90
Psanjay-15 <sanjaypandere26@gmail.com>
2024-05-08 23:16:05 +0530

    Integrated Ui for Login and Sign up

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,16 +1,103 @@
-import { Button } from '@/components/ui/button';
-import { useEffect } from 'react'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-const Auth = () => {
-    useEffect(() => {
+export default function Dashboard() {
+  const [isloginTab, setLoginTab] = useState<boolean>(false);
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:h-screen lg:grid-cols-2 overflow-hidden bg-gradient-to-r from-accent to-white">
+      <div className="flex items-center justify-center py-12">
+        {isloginTab && <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Button
+              variant="link"
+              className="underline p-0"
+              onClick={() => setLoginTab(false)}
+            >
+              Sign up
+            </Button>
+          </div>
+        </div>}
 
-    });
+        {!isloginTab && <div className="mx-auto grid w-[350px] gap-6">
+        <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <p className="text-balance text-muted-foreground">
+            Enter your information to create an account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="Max" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Robinson" required />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Button variant="link" className="underline p-0" onClick={() => setLoginTab(true)}>
+              Sign in
+            </Button>
+          </div>
+        </div>}
+      </div>
 
-    return (
-        <div>
-        <Button>Click</Button>
-        </div>
-    )
+      <div className="hidden bg-muted lg:block ">
+        <img
+          src="https://images.unsplash.com/photo-1511988617509-a57c8a288659?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhhcHB5JTIwcGVvcGxlfGVufDB8fDB8fHww"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
 }
-
-export default Auth
