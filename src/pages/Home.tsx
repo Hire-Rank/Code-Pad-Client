df760c6fb2efe68170706a1e136f0a4a63dce11d
Psanjay-15 <sanjaypandere26@gmail.com>
2024-06-10 23:22:22 +0530

    Footer Done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import MainHome from "@/components/HomeComponents/MainHome";
 import Features from "@/components/HomeComponents/Features";
 import Bottom from "@/components/HomeComponents/Bottom";
+import Footer from "@/components/HomeComponents/Footer";
 const Home = () => {
   return (
     <>
       <MainHome />
       <Features />
       <Bottom />
+      <Footer />
     </>
   );
 };
