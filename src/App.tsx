0f12c72505c945661926159fdb8d239b5ba20d01
Yash-Dabhade <yashdabhade215@gmail.com>
2024-07-03 21:22:16 +0530

    Changes revert back

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import ReactDOM from "react-dom";
 import Auth from "./pages/Auth";
 import CodeEditor from "./pages/CodeEditor";
 import JoinRoom from "./components/Room/JoinRoom";
 import Home from "./pages/Home";
 import GoogleSignInPage from "./pages/GoogleSignInPage";
 
-const RouteWithTrailingSlash = ({ component: Component, ...rest }) => {
-  const location = useLocation();
-  if (location.pathname.substr(-1) !== "/") {
-    return <Navigate to={`${location.pathname}/`} />;
-  }
-  return <Component />;
-};
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/join/:roomId/:roomPassword"
-        element={<RouteWithTrailingSlash component={<JoinRoom />} />}
-      />
+      <Route path="/join/:roomId/:roomPassword" element={<JoinRoom />} />
       <Route path="/auth" element={<Auth />} />
-      <Route
-        path="/codeeditor/:roomId"
-        element={<RouteWithTrailingSlash component={<CodeEditor />} />}
-      />
-      <Route
-        path="/google/:token"
-        element={<RouteWithTrailingSlash component={<GoogleSignInPage />} />}
-      />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/codeeditor/:roomId" element={<CodeEditor />} />
+      <Route path="/google/:token" element={<GoogleSignInPage />} />
     </Routes>
   );
 }
